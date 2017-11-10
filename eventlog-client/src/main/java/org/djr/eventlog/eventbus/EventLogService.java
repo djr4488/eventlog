101c7abe529df8adf7bb5666d7900eb863650148
danny <danny.rucker@ironman.local>
2017-11-10 17:33:55 -0600

    added async event bus not tested

--- eventlog-client/src/main/java/org/djr/eventlog/eventbus/EventLogService.java
+++ eventlog-client/src/main/java/org/djr/eventlog/eventbus/EventLogService.java
@@ -1,24 +1,29 @@
 package org.djr.eventlog.eventbus;
 
+import com.google.common.eventbus.AsyncEventBus;
 import com.google.common.eventbus.EventBus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.enterprise.concurrent.ManagedExecutorService;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
 @ApplicationScoped
 public class EventLogService {
     private Logger log = LoggerFactory.getLogger(EventLogService.class);
-    private EventBus eventBus;
+    private AsyncEventBus eventBus;
     @Inject
     private EventLogListener eventLogListener;
+    @Resource(name = "EventBusManagedExecutor")
+    private ManagedExecutorService managedExecutorService;
 
     @PostConstruct
     public void postConstruct() {
         log.debug("postConstruct() adding event log listener");
-        eventBus = new EventBus();
+        eventBus = new AsyncEventBus("EventLogBus", managedExecutorService);
         eventBus.register(eventLogListener);
     }
 
